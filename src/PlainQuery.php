a1fc116b4135df60c9b09c50bd6f68df5e084795
Shota Nozaki <emonkak@gmail.com>
2015-12-13 13:08:45 +0900

    Refactor PlainQuery

--- src/PlainQuery.php
+++ src/PlainQuery.php
@@ -2,14 +2,51 @@
 
 namespace Emonkak\Orm;
 
-use Emonkak\QueryBuilder\PlainQueryBuilder;
 use Emonkak\QueryBuilder\QueryBuilderInterface;
 use Emonkak\QueryBuilder\ToStringable;
 use Emonkak\QueryBuilder\Chainable;
 
-class PlainQuery extends PlainQueryBuilder implements ExecutableQueryInterface
+class PlainQuery implements ExecutableQueryInterface
 {
     use Chainable;
     use Executable;
     use ToStringable;
+
+    /**
+     * @var string
+     */
+    private $sql;
+
+    /**
+     * @var mixed[]
+     */
+    private $binds;
+
+    /**
+     * @param QueryBuilderInterface $query
+     * @return self
+     */
+    public static function fromQuery(QueryBuilderInterface $query)
+    {
+        list ($sql, $binds) = $query->build();
+        return new self($sql, $binds);
+    }
+
+    /**
+     * @param string  $sql
+     * @param mixed[] $binds
+     */
+    public function __construct($sql, $binds)
+    {
+        $this->sql = $sql;
+        $this->binds = $binds;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function build()
+    {
+        return [$this->sql, $this->binds];
+    }
 }
