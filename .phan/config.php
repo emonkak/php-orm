b5b864bd367342c7017d4dccaaa9947d2438326c
Shota Nozaki <emonkak@gmail.com>
2016-11-24 14:03:02 +0900

    Add phan config

---
+++ .phan/config.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'directory_list' => [
+        'src',
+        'vendor/emonkak/database',
+        'vendor/emonkak/enumerable',
+        'vendor/ocramius/proxy-manager',
+        'vendor/psr/cache',
+    ],
+
+    'exclude_analysis_directory_list' => [
+        'vendor/'
+    ],
+
+    'analyze_signature_compatibility' => false,
+];
