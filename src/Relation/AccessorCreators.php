abc5d32f16342d40d8c78fa2421e487f1c92b9d0
Shota Nozaki <emonkak@gmail.com>
2019-06-12 00:28:50 +0900

    Refactor AccessorCreators

--- src/Relation/AccessorCreators.php
+++ src/Relation/AccessorCreators.php
@@ -15,16 +15,14 @@ final class AccessorCreators
     public static function toKeySelector($key, $class)
     {
         if ($class !== null) {
-            $func = static function($obj) use ($key) {
+            return \Closure::bind(static function($obj) use ($key) {
                 return $obj->$key;
-            };
+            }, null, $class);
         } else {
-            $func = static function($array) use ($key) {
+            return static function($array) use ($key) {
                 return $array[$key];
             };
         }
-
-        return \Closure::bind($func, null, $class);
     }
 
     /**
@@ -35,20 +33,18 @@ final class AccessorCreators
     public static function toPivotKeySelector($key, $class)
     {
         if ($class !== null) {
-            $func = static function($obj) use ($key) {
+            return \Closure::bind(static function($obj) use ($key) {
                 $pivot = $obj->$key;
                 unset($obj->$key);
                 return $pivot;
-            };
+            }, null, $class);
         } else {
-            $func = static function(&$array) use ($key) {
+            return static function(&$array) use ($key) {
                 $pivot = $array[$key];
                 unset($array[$key]);
                 return $pivot;
             };
         }
-
-        return \Closure::bind($func, null, $class);
     }
 
     /**
@@ -59,18 +55,16 @@ final class AccessorCreators
     public static function toKeyEraser($key, $class)
     {
         if ($class !== null) {
-            $func = static function($obj) use ($key) {
+            return \Closure::bind(static function($obj) use ($key) {
                 unset($obj->$key);
                 return $obj;
-            };
+            }, null, $class);
         } else {
-            $func = static function($array) use ($key) {
+            return static function($array) use ($key) {
                 unset($array[$key]);
                 return $array;
             };
         }
-
-        return \Closure::bind($func, null, $class);
     }
 
     /**
@@ -81,22 +75,20 @@ final class AccessorCreators
     public static function toKeyAssignee($key, $class)
     {
         if ($class !== null) {
-            $func = static function($lhs, $rhs) use ($key) {
+            return \Closure::bind(static function($lhs, $rhs) use ($key) {
                 if ($rhs !== null) {
                     $lhs->$key = $rhs;
                 }
                 return $lhs;
-            };
+            }, null, $class);
         } else {
-            $func = static function($lhs, $rhs) use ($key) {
+            return static function($lhs, $rhs) use ($key) {
                 if ($rhs !== null) {
                     $lhs[$key] = $rhs;
                 }
                 return $lhs;
             };
         }
-
-        return \Closure::bind($func, null, $class);
     }
 
     /**
